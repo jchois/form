bb2e851dba0ae68b08236f1098d810b09a3e82f4
juliana <choisjuliana@gmail.com>
2018-06-27 16:48:54 -0500

    clear

--- js/app.js
+++ js/app.js
@@ -1,38 +1,86 @@
 
- 
-
-
 function val(){
     var name = document.getElementById("name-input");
     var lname = document.getElementById("lname-input");
     var email = document.getElementById("email-input");
+    var dateImput = document.getElementById("dt");
     var valid = false;
+
     //if the fields are empty...
-    
     if(valCamp(name)){
-        valido = true;
-    }else{
-        valido = false;
-        document.getElementById("error").style.display = "block";
-        document.getElementById("name-input").style.borderColor = "red";
-        document.getElementById("msj-error").innerHTML = "This field is empty*";
-    }
-    if(valCamp(lname)){
-        valido = true;
+        valid = true;
+        clearError1();
     }else{
         valid = false;
-        document.getElementById("error2").style.display = "block";
-        document.getElementById("lname-input").style.borderColor = "red";
-        document.getElementById("msj-error2").innerHTML = "This field is empty*";
+        fieldError1();
+    }
+    if(valCamp(lname)){
+        valid = true;
+        clearError2();
+    }else{
+        valid = false;
+        fieldError2();
     }
     if(valEmail(email)){
         valid = true;
+        clearFieldEmail()
     }else{
-        document.getElementById("error3").style.display = "block";
-        document.getElementById("email-input").style.borderColor = "red";
-        document.getElementById("msj-error3").innerHTML = "It's not a valid email";
-
+        valid = false;
+        fieldErrorEmail();
     }
+    if(valCamp(dateImput)){
+        valid = true;
+        clearErrorDate()
+    }else{
+        valid = false;
+        fieldErrorDate();
+    }
+
+    return valid;
+
+
+}
+
+function fieldError1(){
+    document.getElementById("error").style.display = "block";
+    document.getElementById("name-input").style.borderColor = "red";
+    document.getElementById("msj-error").innerHTML = "This field is empty*";
+}
+function fieldError2(){
+    document.getElementById("error2").style.display = "block";
+    document.getElementById("lname-input").style.borderColor = "red";
+    document.getElementById("msj-error2").innerHTML = "This field is empty*";
+}
+function fieldErrorEmail(){
+    document.getElementById("error3").style.display = "block";
+    document.getElementById("email-input").style.borderColor = "red";
+    document.getElementById("msj-error3").innerHTML = "It's not a valid email";
+}
+function fieldErrorDate(){
+    document.getElementById("error4").style.display = "block";
+    document.getElementById("dt").style.borderColor = "red";
+    document.getElementById("msj-error4").innerHTML = "It's not a valid date";
+}
+
+function clearError1(){
+    document.getElementById("error").style.display = "none";
+    document.getElementById("name-input").style.borderColor = "";
+    document.getElementById("msj-error").innerHTML = "";
+}
+function clearError2(){
+    document.getElementById("error2").style.display = "none";
+    document.getElementById("lname-input").style.borderColor = "";
+    document.getElementById("msj-error2").innerHTML = "";
+}
+function clearFieldEmail(){
+    document.getElementById("error3").style.display = "none";
+    document.getElementById("email-input").style.borderColor = "";
+    document.getElementById("msj-error3").innerHTML = "";
+}
+function clearErrorDate(){
+    document.getElementById("error4").style.display = "none";
+    document.getElementById("dt").style.borderColor = "";
+    document.getElementById("msj-error4").innerHTML = "";
 }
 
 function valCamp(boxElement){
@@ -52,8 +100,9 @@ function valEmail(){
          }else{
              return false;
          }  
-     }
-    
+     }else{
+     return true;
+    }
 }
 
 function handler(e){
@@ -62,40 +111,53 @@ function handler(e){
 
     
     if(date.getUTCFullYear() > 2000){
-        document.getElementById("drink").style.display = "none";
-        document.getElementById("hobbies").style.display = "none";
-        document.getElementById("like").style.display = "block";
+        
+        nbb();
     }else{
 
         if (date.getUTCFullYear() == 2000){
 
             if(date.getUTCMonth() < dateToday.getMonth()){
                 
-                document.getElementById("drink").style.display = "block";
-                document.getElementById("hobbies").style.display = "block";
-                document.getElementById("like").style.display = "none";
+                bbn();
             }else{
 
                 if(date.getUTCMonth() == dateToday.getMonth()){
 
                     if(date.getUTCDate() <=  dateToday.getDate()){
 
-                        document.getElementById("drink").style.display = "block";
-                        document.getElementById("hobbies").style.display = "block";
-                        document.getElementById("like").style.display = "none"; 
+                        bbn();
+                    }else{
+                        nnb();
                     }
                 }
             }
         }else{
-
-            document.getElementById("drink").style.display = "block";
-            document.getElementById("hobbies").style.display = "block";
-            document.getElementById("like").style.display = "none";
+            if(date.getUTCFullYear() < 2000){
+                bbn();
+            }
+            
         }   
     } 
 }
 
-function drink(){
+function bbn(){
+    document.getElementById("drink").style.display = "block";
+    document.getElementById("hobbies").style.display = "block";
+    document.getElementById("like").style.display = "none"; 
+}
+
+function nbb(){
+    document.getElementById("drink").style.display = "none";
+    document.getElementById("hobbies").style.display = "block";
+    document.getElementById("like").style.display = "block"; 
+}
+
+function drinkYes(){
 
     document.getElementById("smoke").style.display = "block";
+}
+function drinkNo(){
+
+    document.getElementById("smoke").style.display = "none";
 }
